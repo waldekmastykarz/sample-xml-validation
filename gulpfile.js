f1bcd62c6437db57b12373efa3b1e44e92dd3918
Waldek Mastykarz <waldek@mavention.nl>
2015-09-15 08:33:21 +0200

    Switched to libxml-xsd

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,32 @@
 
 var gulp = require('gulp'),
     chalk = require('chalk'),
-    validator = require('xsd-schema-validator');
-
-gulp.task('validate-xml', function () {
-  gulp.src('./*.xml')
-    .pipe(validateXml());
+    fs = require('fs'),
+    xsd = require('libxml-xsd');
+    
+gulp.task('validate-xml1', function() {
+  xsd.parseFile('./manifest.xsd', function(err, schema) {
+    console.log('--schema parsed');
+    schema.validateFile('./manifest-invalid-attribute-value.xml', function(err, validationErrors) {
+      console.log(err);
+      console.log(validationErrors);
+    });
+  });
 });
 
-function validateXml() {
+gulp.task('validate-xml', function () {
+  xsd.parseFile('./manifest.xsd', function(err, schema) {
+    gulp.src('./*.xml')
+      .pipe(validateXml(schema));
+  });
+});
+
+function validateXml(schema) {
   function doValidate(file, cb) {
-    validator.validateXML(file.contents, './manifest.xsd', function (err, result) {
+    schema.validate(file.contents, function(err, validationErrors) {
       console.log('Validation results for ' + chalk.blue(file.path.substring(file.path.lastIndexOf('/')+1)) + ':');
-      if (err) {
-        if (result) {
-          result.messages.forEach(function (m) {
-            console.log(chalk.red(m));
-          });
-        }
-        else {
-          console.log(chalk.red(err));
-        }
-      }
-      else {
-        console.log(chalk.green('OK'));
-      }
-      
-      console.log('\n');
+      console.log(err);
+      console.log(validationErrors);
     });
   }
 
